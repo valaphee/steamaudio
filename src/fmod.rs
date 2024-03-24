340890cc0951b76d1c747565c478433da4dde036
GitGhillie <jillisnoordhoek@gmail.com>
2024-03-24 19:54:52 +0100

    Add fmod SetReverbSource and Terminate

--- src/fmod.rs
+++ src/fmod.rs
@@ -6,34 +6,48 @@ pub fn init_fmod(context: &Context) {
     }
 }
 
-pub fn fmod_set_hrtf(hrtf: &Hrtf) {
+pub fn terminate_fmod() {
+    unsafe {
+        ffi::iplFMODTerminate();
+    }
+}
+
+pub fn set_hrtf(hrtf: &Hrtf) {
     unsafe {
         ffi::iplFMODSetHRTF(hrtf.inner);
     }
 }
 
-pub fn fmod_add_source(source: &Source) -> i32 {
-    unsafe { ffi::iplFMODAddSource(source.inner) }
-}
-
-pub fn fmod_set_simulation_settings(settings: IPLSimulationSettings) {
+pub fn set_simulation_settings(settings: IPLSimulationSettings) {
     unsafe {
         ffi::iplFMODSetSimulationSettings(settings);
     }
 }
 
+pub fn set_reverb_source(source: &Source) {
+    unsafe { ffi::iplFMODSetReverbSource(source.inner) }
+}
+
+pub fn add_source(source: &Source) -> i32 {
+    unsafe { ffi::iplFMODAddSource(source.inner) }
+}
+
+pub fn remove_source(source_handle: i32) {
+    unsafe { ffi::iplFMODRemoveSource(source_handle) }
+}
+
 //todo: Better API for this
 pub fn fmod_create_settings(sampling_rate: u32, frame_size: u32) -> IPLSimulationSettings {
     IPLSimulationSettings {
-        flags: ffi::IPLSimulationFlags_IPL_SIMULATIONFLAGS_DIRECT,
+        flags: ffi::IPLSimulationFlags_IPL_SIMULATIONFLAGS_DIRECT | ffi::IPLSimulationFlags_IPL_SIMULATIONFLAGS_REFLECTIONS,
         sceneType: ffi::IPLSceneType_IPL_SCENETYPE_DEFAULT,
-        reflectionType: 0,
+        reflectionType: ffi::IPLReflectionEffectType_IPL_REFLECTIONEFFECTTYPE_CONVOLUTION,
         maxNumOcclusionSamples: 8,
-        maxNumRays: 32,
-        numDiffuseSamples: 8,
-        maxDuration: 1.0,
+        maxNumRays: 4096,
+        numDiffuseSamples: 32,
+        maxDuration: 2.0,
         maxOrder: 1,
-        maxNumSources: 256,
+        maxNumSources: 8,
         numThreads: 2,
         rayBatchSize: 0,
         numVisSamples: 0,
